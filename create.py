b0135d4d7d6931c948236ba6a36df18c5e885e27
HarvsG <ghjpharvey@gmail.com>
2019-06-02 20:50:27 +0100

    switches to token method (untested)

--- create.py
+++ create.py
@@ -2,17 +2,17 @@ import sys
 import os
 from github import Github
 
-path = "/Users/kalle/Documents/Projects/MyProjects/"
+path = "~/Documents/Projects/"
 
-username = "" #Insert your github username here
-password = "" #Insert your github password here
+with open('myfile.txt') as f:
+    token = f
 
 def create():
     folderName = str(sys.argv[1])
     os.makedirs(path + str(sys.argv[1]))
-    user = Github(username, password).get_user()
-    repo = user.create_repo(sys.argv[1])
-    print("Succesfully created repository {}".format(sys.argv[1]))
+    user = Github(token).get_user()
+    repo = user.create_repo(sys.argv[1], private=True)
+    print("Succesfully created private repository {}".format(sys.argv[1]))
 
 if __name__ == "__main__":
     create()
